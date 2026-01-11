2e3999be70348c7217e3fb2f35ceff6dfe574069
esoys <erik@soysal.de>
2026-01-11 17:45:45 +0100

    add basic metric

--- dist/index.js
+++ dist/index.js
@@ -1,49 +1,38 @@
 import express from "express";
-import type { Request, Response, NextFunction } from "express";
 import { config } from "./config.js";
-
-
 const app = express();
 const PORT = 8080;
-
 app.use(middlewareLogResponses);
 app.use("/metrics", middlewareWriteNumReq);
 app.use("/reset", middlewareMetricReset);
 app.use("/app", middlewareMetricInc, express.static("./src/app"));
-
-app.get("/healthz", (req: Request, res: Response) =>  {
+app.get("/healthz", (req, res) => {
     res.set({
         "Content-Type": "text/plain; charset=utf-8",
     });
     res.status(200).send("OK");
-})
-
+});
 app.listen(PORT, () => {
     console.log(`Server on: http://localhost:${PORT}`);
-})
-
-
-function middlewareLogResponses(req: Request, res: Response, next: NextFunction) {
+});
+function middlewareLogResponses(req, res, next) {
     res.on("finish", () => {
         const statusCode = res.statusCode;
         if (statusCode < 200 || statusCode >= 300) {
             console.log(`[NON-OK] ${req.method} ${req.url} - Status: ${statusCode}`);
         }
-    })
+    });
     next();
 }
-
-function middlewareMetricInc(req: Request, res: Response, next: NextFunction) {
+function middlewareMetricInc(req, res, next) {
     config.fileserverHits++;
     next();
 }
-
-function middlewareWriteNumReq(req: Request, res: Response, next: NextFunction) {
+function middlewareWriteNumReq(req, res, next) {
     res.status(200).send(`Hits: ${config.fileserverHits}`);
     next();
 }
-
-function middlewareMetricReset(req: Request, res: Response, next: NextFunction) {
+function middlewareMetricReset(req, res, next) {
     config.fileserverHits = 0;
     res.status(200).send("Config has been reseted");
     next();
